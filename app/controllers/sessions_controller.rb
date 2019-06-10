98138464173f4e19ee40f15620ef1234475fd80d
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:05:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if session[:name]
+    if !params[:name]
+      redirect_to :login
+    else 
       session[:name] = params[:name]
       redirect_to '/'
-    else
-      redirect_to :login
     end
   end
 
