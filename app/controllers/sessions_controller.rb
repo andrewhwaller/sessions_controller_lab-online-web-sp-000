0680a03f7a4ee7b90a83959df9869865dfbeee28
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:07:27 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     if !params[:name]
-      redirect_to (controller: 'sessions', action: 'new')
+      redirect_to(controller: 'sessions', action: 'new')
     else
       session[:name] = params[:name]
       redirect_to '/'
