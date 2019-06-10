8d2223caba5d05dd988c8820ea4a3b9725dfac3e
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 02:04:06 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:name] = params[:name]
-    redirect_to '/'
+    if session[:name]
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to :login
+      
   end
 
   def destroy
